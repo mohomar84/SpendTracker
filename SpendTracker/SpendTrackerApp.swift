a7f7205cb2a64d4a67a7823e0891ea49aaeed50c
mohomar84 <49999850+mohomar84@users.noreply.github.com>
2023-08-30 21:44:51 +0100

    Initial Commit

---
+++ SpendTracker/SpendTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpendTrackerApp.swift
+//  SpendTracker
+//
+//  Created by Mohamed Omar on 30/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct SpendTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
